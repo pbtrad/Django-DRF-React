b8b160b62a5ae5356b380d0077057931c3f2d028
Paul Browne <paulbrowne2@gmail.com>
2021-06-03 10:14:41 +0100

    Added .env and gitignore to hide secret keys

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0l8zkkj8b(-xla6k6@vz077gwz90%9=o9@k76%lv_*#7gt@f5w'
-
+SECRET_KEY = env(‘SECRET_KEY’)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,7 +79,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env(‘DATABASE_NAME’),
     }
 }
 
